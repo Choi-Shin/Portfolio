494699addbfef57403798353b5f5b280b87394a4
Choi-Shin <shincho1990@naver.com>
2024-06-29 00:29:07 +0900

    d

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("videoChat"));
 app.use("/snakegame", snakegame);
 app.use("/crawler", crawler);
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"))
+  res.sendFile(path.join(__dirname + "/public/index.html"))
 })
 app.get("/snakegame", (req, res) => {
   res.statusCode = 200;
